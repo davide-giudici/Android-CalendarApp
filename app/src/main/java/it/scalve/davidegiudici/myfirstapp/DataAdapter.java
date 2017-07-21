908513c01aa9d78ed159e4fbbac5b58a7d442837
davide-giudici <davide.giudici@cgm.com>
2017-07-21 21:28:12 +0200

    Refactoring costanti dei turni

--- app/src/main/java/it/scalve/davidegiudici/myfirstapp/DataAdapter.java
+++ app/src/main/java/it/scalve/davidegiudici/myfirstapp/DataAdapter.java
@@ -8,39 +8,29 @@ public class DataAdapter {
     private String NOTTE_CORTA = "00:00 - 08:30";
     private String NOTTE_LUNGA = "00:00 - 10:00";
     private String POMERIGGIO = "10:00 - 18:00";
-    private String POMERIGGIO_TARDI = "14:00 - 20:30";
+    private String POME_TARDI = "14:00 - 20:30";
     private String SERA = "18:00 - 00:00";
-    private String EQUIPE = "09:30 - 12:30";
+    private String EQUIPE_MAT = "09:30 - 12:30";
     private String GIORNO = "08:30 - 18:00";
-    private String CASA = "CASA";
+    private String NO_LAVORO = "CASA";
 
     private String[][] turniFissi = new String[][]
     {
-        /*    LUNEDI         MARTEDI             MERCOLEDI                        GIOVEDI          VENERDI                SABATO           DOMENICA */
-                           
-        // I Settimana     
-        {                  
-            CASA,          POMERIGGIO_TARDI,      EQUIPE,                          CASA,           POMERIGGIO,             SERA,             NOTTE_CORTA,
-        },                 
-                           
-        // II Settimana    
-        {                  
-            POMERIGGIO,    CASA,                   EQUIPE,                         CASA,           POMERIGGIO_TARDI,      GIORNO,           CASA,
-        },                 
-                           
-        // III Settimana   
-        {                  
-            CASA,          SERA,                  "00:00 - 12:30" ,                POMERIGGIO,      POMERIGGIO_TARDI,      CASA,              CASA,
-        },                 
-                           
-        // VI Settimana    
-        {                  
-            POMERIGGIO,    POMERIGGIO_TARDI,      EQUIPE + " " + POMERIGGIO_TARDI, CASA,            SERA,                  NOTTE_CORTA,          CASA,
-        },                 
-                           
-        // V Settimana     
-        {                  
-            CASA,          CASA,                  EQUIPE ,                         POMERIGGIO,      SERA,                  NOTTE_CORTA,        GIORNO,
+        /*    LUNEDI         MARTEDI             MERCOLEDI                  GIOVEDI          VENERDI       SABATO           DOMENICA */
+         { // I Settimana
+            NO_LAVORO,      POME_TARDI,     EQUIPE_MAT,                     NO_LAVORO,      POMERIGGIO,     SERA,           NOTTE_CORTA,
+        },
+        { // II Settimana
+            POMERIGGIO,     NO_LAVORO,      EQUIPE_MAT,                     NO_LAVORO,      POME_TARDI,     GIORNO,         NO_LAVORO,
+        },
+        { // III Settimana
+            NO_LAVORO,      SERA,           "00:00 - 12:30" ,               POMERIGGIO,     POME_TARDI,     NO_LAVORO,      NO_LAVORO,
+        },
+        { // VI Settimana
+            POMERIGGIO,     POME_TARDI,     EQUIPE_MAT + " " + POME_TARDI, NO_LAVORO,      SERA,           NOTTE_CORTA,    NO_LAVORO,
+        },
+        { // V Settimana
+            NO_LAVORO,      NO_LAVORO,      EQUIPE_MAT,                     POMERIGGIO,     SERA,           NOTTE_CORTA,    GIORNO,
         },
     };
 
